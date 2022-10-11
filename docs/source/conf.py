2368cb7df2ca6d22564de5b115b7c6cd9e730244
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2022-10-11 21:19:22 +0200

    fix docs again3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,14 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../..'))
+import mock
+
+MOCK_MODULES = ['numpy', 'pandas', 'skimage']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+sys.path.insert(0, os.path.abspath('.'))
+# sys.path.append(os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
